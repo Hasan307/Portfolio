9f1419f5bd063b7c277e77504ef4971987dad6a2
Md Abu Hasan <m.a.hasan307@gmail.com>
2023-12-26 19:40:41 +0600

    images issue slved

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,7 +14,7 @@ const NavBar = () => {
         <span></span>
       </div>
       <div className="text-white absolute my-[10px] mx-8">
-        <img src="src/assets/images/logo.PNG" alt="not found" className="w-[40px]"/>
+        <img src="images/logo.PNG" alt="not found" className="w-[40px]"/>
       </div>
       
       <section className={mobileMenu? "absolute z-10 flex right-[27px] content-end my-10 " : " absolute flex right-[2px] h-14"} >
